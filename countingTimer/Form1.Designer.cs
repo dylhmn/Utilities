dca96b878e3e0ae3a9eb65ccfd03e100825aa788
Dylan Harman <dylharman.18@gmail.com>
2024-11-10 12:44:38 +0000

    Counting timer submission 1

---
+++ countingTimer/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace countingTimer
+{
+    partial class countdownTimer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.topLabel = new System.Windows.Forms.Label();
+            this.timerLabel = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.plus = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.lblDay = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblError = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // topLabel
+            // 
+            this.topLabel.BackColor = System.Drawing.Color.Transparent;
+            this.topLabel.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.topLabel.Location = new System.Drawing.Point(12, 9);
+            this.topLabel.Name = "topLabel";
+            this.topLabel.Size = new System.Drawing.Size(776, 49);
+            this.topLabel.TabIndex = 0;
+            this.topLabel.Text = "COUNTDOWN TIMER";
+            this.topLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timerLabel
+            // 
+            this.timerLabel.BackColor = System.Drawing.Color.Transparent;
+            this.timerLabel.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.timerLabel.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timerLabel.Location = new System.Drawing.Point(276, 120);
+            this.timerLabel.Name = "timerLabel";
+            this.timerLabel.Size = new System.Drawing.Size(249, 49);
+            this.timerLabel.TabIndex = 1;
+            this.timerLabel.Text = "60 SECONDS";
+            this.timerLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.MistyRose;
+            this.btnStop.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnStop.Font = new System.Drawing.Font("Century Gothic", 15F);
+            this.btnStop.Location = new System.Drawing.Point(458, 232);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(112, 50);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "STOP";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.Honeydew;
+            this.btnStart.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnStart.Font = new System.Drawing.Font("Century Gothic", 15F);
+            this.btnStart.Location = new System.Drawing.Point(340, 231);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(115, 50);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnReset.Font = new System.Drawing.Font("Century Gothic", 15F);
+            this.btnReset.Location = new System.Drawing.Point(225, 232);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(112, 50);
+            this.btnReset.TabIndex = 5;
+            this.btnReset.Text = "RESET";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // plus
+            // 
+            this.plus.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.plus.Font = new System.Drawing.Font("Century Gothic", 15F);
+            this.plus.Location = new System.Drawing.Point(406, 287);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(49, 34);
+            this.plus.TabIndex = 6;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // minus
+            // 
+            this.minus.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.minus.Font = new System.Drawing.Font("Century Gothic", 15F);
+            this.minus.Location = new System.Drawing.Point(340, 287);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(49, 34);
+            this.minus.TabIndex = 7;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            this.minus.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSave.Font = new System.Drawing.Font("Century Gothic", 15F);
+            this.btnSave.Location = new System.Drawing.Point(225, 287);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(112, 32);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "SAVE";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLoad.Font = new System.Drawing.Font("Century Gothic", 15F);
+            this.btnLoad.Location = new System.Drawing.Point(458, 289);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(112, 32);
+            this.btnLoad.TabIndex = 9;
+            this.btnLoad.Text = "LOAD";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.BackColor = System.Drawing.SystemColors.Control;
+            this.progressBar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.progressBar.Location = new System.Drawing.Point(225, 216);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(345, 10);
+            this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar.TabIndex = 10;
+            // 
+            // lblDay
+            // 
+            this.lblDay.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.lblDay.AutoSize = true;
+            this.lblDay.Font = new System.Drawing.Font("Century Gothic", 15F);
+            this.lblDay.Location = new System.Drawing.Point(703, 9);
+            this.lblDay.Name = "lblDay";
+            this.lblDay.Size = new System.Drawing.Size(31, 23);
+            this.lblDay.TabIndex = 11;
+            this.lblDay.Text = "---";
+            this.lblDay.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblTime
+            // 
+            this.lblTime.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Century Gothic", 15F);
+            this.lblTime.Location = new System.Drawing.Point(15, 9);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(31, 23);
+            this.lblTime.TabIndex = 12;
+            this.lblTime.Text = "---";
+            this.lblTime.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // lblError
+            // 
+            this.lblError.BackColor = System.Drawing.Color.Transparent;
+            this.lblError.Font = new System.Drawing.Font("Century Gothic", 15F);
+            this.lblError.ForeColor = System.Drawing.Color.Maroon;
+            this.lblError.Location = new System.Drawing.Point(121, 335);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(558, 49);
+            this.lblError.TabIndex = 13;
+            this.lblError.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // countdownTimer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.lblDay);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.timerLabel);
+            this.Controls.Add(this.topLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "countdownTimer";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label topLabel;
+        private System.Windows.Forms.Label timerLabel;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label lblDay;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblError;
+    }
+}
+
---
+++ countingTimer/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace countingTimer
+{
+    public partial class countdownTimer : Form
+    {
+        // VARIABLES
+        DateTime stasisTime = DateTime.Now; // Gets the current time immediately
+        int timeLeft = 60; 
+        int timeSaved = 0; // For saving & loading
+        int errorMessageCountdown = -1; // Error handling message screen time
+        bool Error = false;
+
+        public countdownTimer()
+        {
+            InitializeComponent();
+            lblDay.Text = stasisTime.ToString("d/MM/yy");
+            lblTime.Text = stasisTime.ToString("HH:mm:ss");
+            timer1.Start();
+            // Gets the immediate time values so the displays have something & starts the SECOND timer
+            // .. So the latter can update in real time
+        }
+        private void timer1_Tick(object sender, EventArgs e)// Note: 1000 Ticks is a second
+        {
+            if (timeLeft > 0)
+            {
+                timeLeft--;
+                progressBar.Value++;
+                timerLabel.Text = timeLeft + " SECONDS";
+            }
+            else {
+                timer.Stop();
+                timerLabel.Text = "TIME IS UP";
+            }
+        }
+        private void timer1_Tick_1(object sender, EventArgs e)
+            // timer number 2, one that doesn't need the "start" button to tick down.
+        {
+            DateTime constantTime = DateTime.Now;
+            lblTime.Text = constantTime.ToString("HH:mm:ss"); // it will constantly be updating
+            if (Error == true)
+            {
+                lblError.Text = "AN ERROR HAS OCCURED - THE TIMER HAS BEEN RESET";
+                Thread.Sleep(5500);
+                lblError.Text = null;
+                Error = false;
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            timeLeft = 60;
+            timerLabel.Text = timeLeft + " SECONDS";
+
+            progressBar.Value = 0;
+            progressBar.Maximum = 60;
+
+            // Reset button. Doesn't impact the save feature.
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer.Start(); // Visual studio accounts for a lot of the logic required for time
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+        }
+
+        private void minus_Click(object sender, EventArgs e)
+        {
+            try // Time can't deal with negatives /This error handling catches it
+            {
+                timeLeft -= 5;
+                progressBar.Maximum = timeLeft;
+                progressBar.Value = 0;
+
+                if (timeLeft <= 5)
+                {
+                    timeLeft = 5;
+                    progressBar.Maximum = 5;
+                }
+                timerLabel.Text = timeLeft.ToString() + " SECONDS";
+            }
+            catch {
+                // Reset...
+                timer.Stop();
+                timeLeft = 60;
+                timerLabel.Text = timeLeft + " SECONDS";
+                progressBar.Value = 0;
+                progressBar.Maximum = 60;
+                // Error message...
+                Error = true;
+                lblError.Text = "AN ERROR HAS OCCURED - THE TIMER HAS BEEN RESET";
+
+            }
+        }
+
+        private void plus_Click(object sender, EventArgs e)
+        {
+            timeLeft += 5;
+            progressBar.Maximum = timeLeft;
+            progressBar.Value = 0;
+            if (timeLeft >= 1000) { 
+                timeLeft = 1000;
+            }
+            timerLabel.Text = timeLeft.ToString() + " SECONDS";
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            timeSaved = timeLeft;
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            progressBar.Value = 0;
+            progressBar.Maximum = timeSaved;
+            timeLeft = timeSaved;
+            timerLabel.Text = timeLeft.ToString() + " SECONDS";
+        }
+    }
+}
